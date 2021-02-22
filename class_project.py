57f5694230b2cc746fdb5f7480fede4a442db63e
rsiv <rwsivadon@my365.bellevue.edu>
2021-02-22 15:25:52 -0800

    added ask_to_continue() and cleaned up main() to have message print befor try block. Removed call to function from except block to avoid exceptins in exceptions

--- class_project.py
+++ class_project.py
@@ -18,18 +18,11 @@ def zip_funct():
 
     if data["cod"] != "404": 
         #Checks for successful connection
-        print(f"Connected to weather services for zip code: {u_zip}")
+        print(f"Connected to weather services for zip code: {u_zip}.")
         print_funct(data)
+        ask_to_continue()
     else:
         print_funct(data) #if 404 is found, runs print function to check for "key error" that will run the except code in the try block located in main()
-
-    search=input("Would you like to search again? type 'y' for Yes or 'n' for No: ").lower()
-    if search == 'y' or search == 'yes':
-        main()
-    if search == 'n' or search == 'no':
-        print('Good bye! Thank you for using Python weather services')
-        exit()
-
 
 def city_funct():
     #City funtion for when user wantes to search by city 
@@ -43,17 +36,10 @@ def city_funct():
         #Checks for successful connection
         print(f"\nConnected to {u_city} weather services! ")
         print_funct(data)
+        ask_to_continue()
     else:
         print_funct(data) #if 404 is found, runs print function to check for "key error" that will run the except code in the try block located in main()
-    
-    search=input("Would you like to search again? type 'y' for Yes or 'n' for No: ").lower()
-    if search == 'y' or search == 'yes':
-        main()
-    if search == 'n' or search == 'no':
-        print('Good bye! Thank you for using Python weather services')
-        exit()
-    
-
+     
 def print_funct(data):
     #takes information stored in data varible and stores them in specific varibles that will be printed
     description = data['weather'][0]['description']
@@ -80,6 +66,18 @@ def print_funct(data):
     wind = data['wind']['speed']
     print(f'\tWind Speed: {wind} mph\n')
 
+def ask_to_continue():
+    # Asks user if they want to search again.
+    while True:
+        search=input("Would you like to search again? type 'y' for Yes or 'n' for No: ").lower()
+        if search == 'y' or search == 'yes':
+            main()
+            break
+        elif search == 'n' or search == 'no':
+            print('\nGood bye! Thank you for using Python weather services!')
+            exit()
+        else:
+            print("\nError! Not a valid option please try again.\n")
     
 # Main function
 def main():
@@ -90,19 +88,17 @@ def main():
         user_selection = input("If you would like to search by city please, type 'city'"
         " if you perfer to search by zip code type 'zip': ").lower()
         if user_selection == 'city':
+            print('\nSearching by city name...')
             try:
-                print('\nSearching by city name...')
                 city_funct()
             except KeyError:
-                print("\n---Error---\nConnention aborted, city not found. Please try again!\n")
-                city_funct()
+                print("\n---Error---\nConnention aborted, city not found. Please try again.\n")
         elif user_selection == 'zip':
+            print('\nSearching by zip code...')
             try:
-                print('\nSearching by zip code...')
                 zip_funct()
             except KeyError:
-                print("\n---Error---\nConnention aborted, zip code not found. Please try again!\n")
-                zip_funct()
+                print("\n---Error---\nConnention aborted, zip code not found. Please try again.\n")
         else: 
             print("\nERROR! Search option is unavailable please search by 'city' or ' zip'.\n")        
 main()
